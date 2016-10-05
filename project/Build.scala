4581827867a23c3bd7e09e414ebe1d4ad4eaf67e
kanshi <andrzej.tucholka@gmail.com>
2016-10-05 12:45:23 +0200

    Initial commit

---
+++ project/Build.scala
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2016. All rights reserved by Andrzej Tuchołka (andrzej@certo.co)
+ */
+
+object Build extends sbt.Build {
+
+  import sbt._
+
+  lazy val root = Project("LW3D", file("."))
+
+}
